5cf7df85582d207b11c6c47f22f7ef482d653f8a
sinksky <sink.sky@qq.com>
2022-09-04 17:03:05 +0800

    [refact] new category

--- PowerShell/init.ps1
+++ PowerShell/init.ps1
@@ -1,6 +1,9 @@
+# First
+# winget install Microsoft.PowerShell
+
 Install-Module posh-git -Scope CurrentUser
 Install-Module oh-my-posh -Scope CurrentUser -RequiredVersion 2.0.496
 Install-Module z -Scope CurrentUser
 
 $script_path = Split-Path -Parent $MyInvocation.MyCommand.Definition
-cp $script_path/../profile.ps1 $PROFILE
+cp $script_path/profile.ps1 $PROFILE
--- WindowsTerminal/init_wt.ps1
+++ WindowsTerminal/init_wt.ps1
@@ -1,1 +1,3 @@
+# actions & prfiles.defaults & scheme is Important !
+
 $setting_path = %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
